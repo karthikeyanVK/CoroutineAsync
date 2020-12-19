af8a46372099591daba96b44e1adb226a5f4f7e6
Admin <Admin@KARTHIKEYAN-VK>
2020-12-19 09:33:49 +0530

    added gitignore

---
+++ CoroutineAsync/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CoroutineAsync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            //Consumer();
+            ConsumerAsync().GetAwaiter().GetResult();
+
+        }
+
+        public static async Task ConsumerAsync()
+        {
+            await foreach (int i in RangeAsync(1, 10))
+            {
+                await Task.Delay(i); //Network Calls
+                Console.WriteLine(i.ToString());
+            }
+        }
+
+
+        static async IAsyncEnumerable<int> RangeAsync(int start, int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                await Task.Delay(i); //Network Calls
+                yield return start + i;
+            }
+        }
+
+        public static void Consumer()
+        {
+            foreach (int i in Integers())
+            {
+                Console.WriteLine(i.ToString());
+            }
+        }
+
+        public static IEnumerable<int> Integers()
+        {
+            yield return 1;
+            yield return 2;
+            yield return 4;
+            yield return 8;
+            yield return 16;
+            yield return 16777216;
+        }
+    }
+}
